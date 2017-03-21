c8822f2b781a87bd5262cc62e9f61c68ca60e3e0
polang <grzx2210@163.com>
2017-03-21 09:53:14 +0800

    [rebirth-admin] menu setup

--- src/app/manage/manage-app.component.ts
+++ src/app/manage/manage-app.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import * as Immutable from 'immutable';
+import { menuBar } from './meun-options';
 
 @Component({
   selector: 'app-public-app',
@@ -7,73 +9,7 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ManageAppComponent implements OnInit {
-  menus = {
-    logo: 'http://greengerong.com/rebirth/assets/img/wolf2.png',
-    title: '破狼博客',
-    home: './',
-    menus: [
-      {
-        text: '@Rebirth/UI',
-        router: ['./'],
-        icon: 'glyphicon glyphicon-home'
-      },
-      {
-        text: 'Components',
-        children: [],
-        icon: 'glyphicon glyphicon-fire'
-      },
-      {
-        text: 'API Docs',
-        url: '/ng2-rebirth-ui/compodocs/overview.html',
-        target: '_blank',
-        icon: 'glyphicon glyphicon-heart'
-      },
-      {
-        text: '@Github',
-        url: 'https://github.com/greengerong/ng2-rebirth-ui',
-        target: '_blank',
-        icon: 'glyphicon glyphicon-user'
-      },
-      {
-        text: 'Themes',
-        icon: 'glyphicon glyphicon-cog',
-        children: [
-          {
-            text: 'Default',
-            url: './?theme=Default'
-          },
-          {
-            text: 'Dark',
-            url: './?theme=Dark'
-          },
-          {
-            text: 'Cosmo',
-            url: './?theme=Cosmo'
-          },
-          {
-            text: 'Paper',
-            url: './?theme=Paper'
-          },
-          {
-            text: 'Journal',
-            url: './?theme=Journal'
-          },
-          {
-            text: 'Readable',
-            url: './?theme=Readable'
-          },
-          {
-            text: 'United',
-            url: './?theme=United'
-          },
-          {
-            text: 'Sandstone',
-            url: './?theme=Sandstone'
-          }
-        ]
-      }
-    ]
-  };
+  menus = menuBar;
 
   constructor() {
   }
