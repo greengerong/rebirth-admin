0a7c6d9d550dc1cd5a88cfbbdecb9e8346b5b718
wfsovereign <wfsovereign@outlook.com>
2017-06-25 00:40:03 +0800

    [fyqj] #1 init page redirect to login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 
 
 export const ROUTER_CONFIG: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/public/home' },
+  { path: '', pathMatch: 'full', redirectTo: '/public/login' },
   { path: 'public', loadChildren: 'app/public/public-app.module#PublicAppModule' },
   { path: 'manage', loadChildren: 'app/manage/manage-app.module#ManageAppModule' },
 ];
--- src/app/public/public-app.routes.ts
+++ src/app/public/public-app.routes.ts
@@ -1,12 +1,14 @@
 import { Routes } from '@angular/router';
 import { PublicAppComponent } from './public-app.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
 
 export const ROUTER_CONFIG: Routes = [
   {
     path: '', component: PublicAppComponent,
     children: [
-      { path: '', pathMatch: 'full', redirectTo: '/public/home' },
+      { path: '', pathMatch: 'full', redirectTo: '/public/login' },
+      { path: 'login', component: LoginComponent },
       { path: 'home', component: HomeComponent },
     ]
   }
