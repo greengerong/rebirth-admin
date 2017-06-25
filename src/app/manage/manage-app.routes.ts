c3fdb4b72fcc640deecbe0ae70bbfe1d6052fbae
polang <grzx2210@163.com>
2017-06-25 12:54:07 +0800

    [rebirth-admin]  rebirth-permission setup

--- src/app/manage/manage-app.routes.ts
+++ src/app/manage/manage-app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { ManageAppComponent } from './manage-app.component';
+import { AuthLoginPermission } from 'rebirth-permission';
 
 export const ROUTER_CONFIG: Routes = [
   {
-    path: '', component: ManageAppComponent,
+    path: '',
+    component: ManageAppComponent,
+    canActivate: [AuthLoginPermission,],
     children: [
+
+      // can use role AuthRolePermission
       // { path: '', pathMatch: 'full', redirectTo: '/manage/home' },
     ]
   }
