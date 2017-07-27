fd2bac90b53278ef998f9693e1c4c0fc3b1f42de
polang <grzx2210@163.com>
2017-07-28 00:22:45 +0800

    [rebirth-admin] remove theme

--- src/app/manage/manage-app.component.ts
+++ src/app/manage/manage-app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { menuBar } from './meun-options';
 
 @Component({
   selector: 'app-manage-app',
@@ -7,6 +6,34 @@ import { menuBar } from './meun-options';
   styleUrls: ['./manage-app.component.scss']
 })
 export class ManageAppComponent {
-  menus = menuBar;
+  menus = {
+    logo: '/assets/icon/favicon-96x96.png',
+    title: '破狼博客',
+    home: './',
+    menus: [
+      {
+        text: '@Rebirth/UI',
+        router: ['./'],
+        icon: 'glyphicon glyphicon-home'
+      },
+      {
+        text: 'Components',
+        children: [],
+        icon: 'glyphicon glyphicon-fire'
+      },
+      {
+        text: 'API Docs',
+        url: '/ng2-rebirth-ui/compodocs/overview.html',
+        target: '_blank',
+        icon: 'glyphicon glyphicon-heart'
+      },
+      {
+        text: '@Github',
+        url: 'https://github.com/greengerong/ng2-rebirth-ui',
+        target: '_blank',
+        icon: 'glyphicon glyphicon-user'
+      }
+    ]
+  };
 
 }
