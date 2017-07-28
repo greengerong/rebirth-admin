ac31436dbda87c088fc0058ef97caf22a057d26a
polang <grzx2210@163.com>
2017-07-28 23:46:14 +0800

    [rebirth-admin] login page style on mobile

--- src/app/core/storage/storage.service.spec.ts
+++ src/app/core/storage/storage.service.spec.ts
@@ -1,15 +1,27 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { inject } from '@angular/core/testing';
+import { ReStorageService } from './storage.service';
+import { TestBedUtils } from '../../../test-utils/test-bed-utils';
+import { StorageService } from 'rebirth-storage';
 
-import { StorageService } from './storage.service';
 
-describe('StorageService', () => {
+describe('ReStorageService', () => {
   beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [StorageService]
+    TestBedUtils.configureTestingModule({
+      providers: [ReStorageService]
     });
   });
 
-  it('should be created', inject([StorageService], (service: StorageService) => {
-    expect(service).toBeTruthy();
-  }));
+  it('should storage data', inject([ReStorageService, StorageService],
+    (service: ReStorageService, storageService: StorageService) => {
+      const key = 'key';
+      const value = { test: true };
+      storageService.put = jasmine.createSpy('put', () => null);
+
+      service.save(key, value);
+
+      expect(storageService.put).toHaveBeenCalled();
+
+      service.remove(key);
+      service.clear();
+    }));
 });
