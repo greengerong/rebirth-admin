b6c8ac853aa6ac7cbabd83d7d88f78580dd01e03
wfsovereign <wfsovereign@outlook.com>
2017-10-24 21:51:42 +0800

    [fyqj] #4 menu bar support horizontal expand

--- src/app/manage/manage-app.component.ts
+++ src/app/manage/manage-app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
@@ -6,7 +6,7 @@ import { ActivatedRoute, Router } from '@angular/router';
   templateUrl: './manage-app.component.html',
   styleUrls: ['./manage-app.component.scss']
 })
-export class ManageAppComponent {
+export class ManageAppComponent implements OnInit {
   menus = {
     logo: '/assets/icon/favicon-96x96.png',
     title: '破狼博客',
--- src/app/shared/menu-bar/menu-bar.component.ts
+++ src/app/shared/menu-bar/menu-bar.component.ts
@@ -1,15 +1,24 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { MenuBarService } from './menu-bar.service';
+import { WindowRef } from 'rebirth-ng';
 
 @Component({
   selector: 'app-menu-bar',
   templateUrl: './menu-bar.component.html',
-  styleUrls: ['./menu-bar.component.scss']
+  styleUrls: ['./menu-bar.component.scss'],
+  host: {
+    '[class]': 'isOpen ? "open-menu" : "hide-menu"'
+  },
+  exportAs: 'menuBar'
 })
 export class MenuBarComponent implements OnInit {
+  static MAX_MIDDLE_SCREEN = 768;
   @Input() configs;
 
-  constructor(private menuBarService: MenuBarService) {
+  @Input() isOpen: boolean;
+
+  constructor(private menuBarService: MenuBarService, windowRef: WindowRef) {
+    this.isOpen = windowRef.innerWidth >= MenuBarComponent.MAX_MIDDLE_SCREEN;
   }
 
   ngOnInit(): void {
@@ -26,6 +35,10 @@ export class MenuBarComponent implements OnInit {
 
   shouldShowElement(path): boolean {
     return this.menuBarService.isStartWithCurrentPath(path);
+  }
+
+  toggle() {
+    this.isOpen = !this.isOpen;
   }
 
 }
