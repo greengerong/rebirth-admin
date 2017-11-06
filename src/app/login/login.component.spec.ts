602f6238414781fa105c8edd22d601159582606d
polang <grzx2210@163.com>
2017-11-06 16:02:00 +0800

    [rebirth-admin] login service to feature module

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -4,6 +4,7 @@ import { LoginComponent } from './login.component';
 import { RouterModule } from '@angular/router';
 import { APP_BASE_HREF } from '@angular/common';
 import { TestBedUtils } from '../../test-utils/test-bed-utils';
+import { LoginService } from './login.service';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -13,7 +14,7 @@ describe('LoginComponent', () => {
     TestBedUtils.configureTestingModule({
       imports: [RouterModule.forRoot([])],
       declarations: [LoginComponent],
-      providers: [{ provide: APP_BASE_HREF, useValue: '/' }]
+      providers: [LoginService, { provide: APP_BASE_HREF, useValue: '/' }]
     })
       .compileComponents();
   }));
