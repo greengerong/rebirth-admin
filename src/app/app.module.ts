2e80f8f23cfabbb05940e6a8ff54d60062931b7b
polang <grzx2210@163.com>
2017-11-12 21:00:35 +0800

    [rebirth-admin] prettier

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { RouterModule } from "@angular/router";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
-import { AppComponent } from "./app.component";
-import { ROUTER_CONFIG } from "./app.routes";
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { CoreModule } from "./core/core.module";
-import { SharedModule } from "./shared/shared.module";
+import { AppComponent } from './app.component';
+import { ROUTER_CONFIG } from './app.routes';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
@@ -15,9 +15,9 @@ import { SharedModule } from "./shared/shared.module";
     BrowserAnimationsModule,
     CoreModule,
     SharedModule,
-    RouterModule.forRoot(ROUTER_CONFIG)
+    RouterModule.forRoot(ROUTER_CONFIG),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
