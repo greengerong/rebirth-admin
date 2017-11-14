0295bcfc8d86f962f9886e499d18d1ae9348ad43
polang <grzx2210@163.com>
2017-11-14 10:18:11 +0800

    [#0][greengerong] check commit message

--- build/commit-msg.js
+++ build/commit-msg.js
@@ -5,15 +5,12 @@ const commitMessage = fs.readFileSync(path.join('.git', 'COMMIT_EDITMSG'), 'utf8
 const commitRegex = /^(\[((#[0-9]+)|(TECH))\]\[\S+(\s\&\s\S+)*\]|merge)\s/g;
 const errorMsg = "Aborting commit. Commit message must like '[#100][greengerong] commit message' or '[TECH][greengerong] commit message'";
 
-const check = commitRegex.test(commitMessage);
-console.log(commitMessage, check);
-if (check) {
-  console.log('success macth');
-  process.exit(0);
-} else {
+if (!commitRegex.test(commitMessage)) {
   console.error('error1:' + errorMsg);
   process.exit(1);
+  return;
 }
+process.exit(0);
 
 
 
