13beee4a829fc4e919a5a2da6e69d75cd0549a9c
polang <grzx2210@163.com>
2017-12-09 11:17:06 +0800

    [TECH][greengerong] angular server worker

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,17 @@ import { ROUTER_CONFIG } from './app.routes';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
+import { environment } from '../environments/environment';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    ServiceWorkerModule.register('/ngsw-worker.js', {
+      enabled: environment.production,
+    }),
     CoreModule,
     SharedModule,
     RouterModule.forRoot(ROUTER_CONFIG),
