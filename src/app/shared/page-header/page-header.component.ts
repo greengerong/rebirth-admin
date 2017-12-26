4340edfedfccede3374be24fb244e0fcd84bc7e1
wfsovereign <wfsovereign@outlook.com>
2017-12-26 22:54:26 +0800

    [#9][fyqj] add user relative info in header

--- src/app/shared/page-header/page-header.component.ts
+++ src/app/shared/page-header/page-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-page-header',
@@ -7,6 +7,9 @@ import { Component, EventEmitter, Output } from '@angular/core';
 })
 export class PageHeaderComponent {
   @Output() onExpand = new EventEmitter();
+
+  @ViewChild('volumeTemplate') volumeTemplate;
+  dummyName = '管理员名称';
 
   handleExpandClick() {
     this.onExpand.emit();
